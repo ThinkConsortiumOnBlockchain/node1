fe2c60e2fe0a2aebd31c3e83792ce6f4a05a0c73
Mike <mike@thinkathon.io>
2017-01-10 13:18:50 +0900

    yol

--- app.js
+++ app.js
@@ -213,7 +213,7 @@ console.log(tx);
 //originalNonce = 0;
 
 
-var rawTx = {"nonce":"0x"+nonce,"gasPrice":"0x04e3b29200","gasLimit":"0x5208","to":"0x"+toAddr,"value":amount,"data":""};
+var rawTx = {"nonce":"0x"+nonce,"gasPrice":"0x098bca5a00","gasLimit":"0x5208","to":"0x"+toAddr,"value":amount,"data":"", "chainId":1};
 /*
             var rawTx = {
   nonce: '0x00',
